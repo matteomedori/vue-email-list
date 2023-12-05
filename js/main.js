6a5e900275fc284f83315458f614063f44ab8000
matteomedori <matteo.medori.97@gmail.com>
2023-12-05 15:20:34 +0100

    feat: genera 10 mail nel dom

--- js/main.js
+++ js/main.js
@@ -5,7 +5,18 @@ const { createApp } = Vue;
 createApp({
   data() {
     return {
+      apiUrl: "https://flynn.boolean.careers/exercises/api/random/mail",
       message: "Hello Vue!",
+      emails: [],
     };
+  },
+  methods: {},
+  mounted() {
+    for (let i = 0; i < 10; i++) {
+      axios.get(this.apiUrl).then((response) => {
+        console.log(response.data.response);
+        this.emails.push(response.data.response);
+      });
+    }
   },
 }).mount("#app");
